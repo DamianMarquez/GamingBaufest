686fc2bef9506b11665637e5d52774969ac75582
Damian <damian_1986_2@hotmail.com>
2020-08-23 11:49:52 -0300

    Movimiento Base / Cubo a Esfera

---
+++ Assets/Script/Core/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+        
+    }
+}
