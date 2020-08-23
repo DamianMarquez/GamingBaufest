49713e218b7eebd158dec5a6c2b4b6a010e68f1b
Damian <damian_1986_2@hotmail.com>
2020-08-23 18:51:33 -0300

    Creacion del "PlayerController"

--- Assets/Script/Core/Mover.cs
+++ Assets/Script/Core/Mover.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MoverMouse : MonoBehaviour
+public class Mover : MonoBehaviour
 {
 
     [SerializeField] Transform target;
@@ -15,25 +15,16 @@ public class MoverMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            
-            MoveToCursor();
-        }
-        
+       
         UpdateAnimator();
-        
+       
     }
 
-    private void MoveToCursor(){
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit=  Physics.Raycast(ray, out hit);
 
-        if(hasHit){
-            GetComponent<NavMeshAgent>().destination = hit.point;    
-        }
-        UnityEngine.Debug.DrawRay(ray.origin, ray.direction * 100);
+
+    public void MoveTo(Vector3 destination)
+    {
+        GetComponent<NavMeshAgent>().destination = destination;
     }
 
     private void UpdateAnimator(){
