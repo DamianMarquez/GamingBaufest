a5d85376eae939678bbe12a2a98be95104f993df
Daniel Santiago Panelo <dpanelo@baufest.com>
2020-11-02 16:25:52 -0300

    Sistema rudimentario de combate

--- Assets/Script/Combat/CombatTarget.cs
+++ Assets/Script/Combat/CombatTarget.cs
@@ -1,7 +1,32 @@
 using UnityEngine;
 
-    namespace RPG.Combat{
+namespace RPG.Combat{
     public class CombatTarget : MonoBehaviour {
-        
+
+        private float vida = 100;
+        public Armadura armaduraEquipada = new ArmaduraCuero();
+
+        public void AplicarDanio(float danio){
+            vida = vida-danio;
+
+            Debug.Log("Vida restante =" + vida);
+            
+            ChequearMuerte();
+        }
+
+        private void ChequearMuerte(){
+            if(vida <= 0.0f){
+                Debug.Log("Muerto");
+            }
+        }
+    }
+
+    //Mover a archivo aparte o hacer scriptable object.
+
+    public class ArmaduraCuero : Armadura {
+        public ArmaduraCuero(){
+            evasion = 30.0f;
+            defensaFisica = 2.0f;
+        }
     }
 }
--- Assets/Script/Combat/Fighter.cs
+++ Assets/Script/Combat/Fighter.cs
@@ -4,13 +4,86 @@ using UnityEngine;
 
 namespace RPG.Combat
 {
-    
     public class Fighter : MonoBehaviour
     {
+        public Arma armaEquipada = new Espada();    
 
         public void Attack(CombatTarget target){
             print ("Atacando");
+            
+            if(ObjetivoEnRango(target)){
+                if(AtaquePega(target)){
+                    var danio = CalcularDanio(target);
+
+                    target.AplicarDanio(danio);
+                }
+            }
+            else
+            {
+                Debug.Log("Fuera de rango");
+            }
+            //Si dentro de rango y paso el cooldown de velocidad, 
+            //intentar atacar
+
+            
+
+            //Calcular posibilidad de ataque player vs evasion target
+
+            //Si pega, calcular daño (azar?) player vs reduccion target
+            //Restar daño a target
         }
 
+        private bool ObjetivoEnRango(CombatTarget target){
+            var distance = Vector3.Distance(target.transform.position, this.transform.position);
+
+            Debug.Log("Distancia:" + distance);
+            Debug.Log("Rango arma:" + armaEquipada.rango);
+
+            if(distance <= armaEquipada.rango){
+                Debug.Log("En Rango");
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public bool AtaquePega(CombatTarget target){
+            var tiradaDado = Random.Range(1.0f, 100.0f);
+
+            if (tiradaDado <= armaEquipada.posibilidadAtaque - target.armaduraEquipada.evasion){
+                return true;
+            }
+
+            Debug.Log("Fallo");
+            return false;
+        }
+
+        public float CalcularDanio(CombatTarget target){
+           var danio = Random.Range(armaEquipada.danioMinimo, armaEquipada.danioMaximo);
+
+           var danioTotal = danio - target.armaduraEquipada.defensaFisica;
+
+            if (danioTotal < 0){
+                danioTotal = 0;
+            }
+
+            Debug.Log("Danio = " + danioTotal);
+
+            return danioTotal;
+        }
+
+    }
+
+    //Mover a archivo aparte, o hacer scriptable object.
+    public class Espada : Arma
+    {
+        public Espada(){
+            posibilidadAtaque = 75.0f;
+            velocidadAtaque = 3.0f;
+            rango = 2.0f;
+            danioMaximo = 5.0f;
+            danioMinimo = 1.0f;
+        }
     }
 }
