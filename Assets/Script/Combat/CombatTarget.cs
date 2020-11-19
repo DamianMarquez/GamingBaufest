14a876105ea05ea1147ff8421dddb6ffb7a42f85
Gabriel Ajaya <ajaya_gabriel@hotmail.com>
2020-11-19 01:19:02 -0300

    Se muestra barra de vida que va reduciendose segun el porcentaje de vida actual.

--- Assets/Script/Combat/CombatTarget.cs
+++ Assets/Script/Combat/CombatTarget.cs
@@ -1,21 +1,38 @@
+using System;
 using UnityEngine;
 
 namespace RPG.Combat{
     public class CombatTarget : MonoBehaviour {
 
-        private float vida = 100;
+        private float vidaActual;
         public Armadura armaduraEquipada = new ArmaduraCuero();
+        public float vidaMaxima = 100;
+        public event Action<float> CambioPorcentajeVida = delegate { };
+
+        private void OnEnable()
+        {
+            vidaActual = vidaMaxima;
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                this.AplicarDanio(10);
+            }    
+        }
 
         public void AplicarDanio(float danio){
-            vida = vida-danio;
-
-            Debug.Log("Vida restante =" + vida);
+            vidaActual = vidaActual - danio;
+            float porcentajeVidaActual = vidaActual / vidaMaxima;
+            CambioPorcentajeVida(porcentajeVidaActual);
+             
+            Debug.Log("Vida restante =" + vidaActual);
             
             ChequearMuerte();
         }
 
         private void ChequearMuerte(){
-            if(vida <= 0.0f){
+            if(vidaActual <= 0.0f){
                 Debug.Log("Muerto");
             }
         }
